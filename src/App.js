a930a3cc3608d35eb60773312ae8a2431effb597
Muhannad1989 <muhannadjudah@gmail.com>
2024-12-18 08:46:32 +0100

    add bedrock js files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import { useState } from "react";
+import ChatInput from "./components/ChatInput/ChatInput";
+import ChatMessage from "./components/ChatMessage/ChatMessage";
+import "./App.css";
 
-function App() {
+const MODEL_NAME = "Claude";
+const USER_NAME = "User";
+
+const App = () => {
+  const [history, setHistory] = useState([
+    { author: USER_NAME, text: "Hello!" },
+    { author: MODEL_NAME, text: "How can I help you?" }
+  ]);
+
+  const onSubmit = async prompt => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col h-screen p-4">
+      <div className="overflow-y-scroll flex-1">
+        {history.map(({ author, text }) => (
+          <ChatMessage
+            author={author}
+            reverse={author === USER_NAME}
+            text={text}
+          />
+        ))}
+      </div>
+
+      <div className="flex items-center justify-between mt-auto h-20 sticky bottom-0 left-0 right-0">
+        <ChatInput onSubmit={onSubmit} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
